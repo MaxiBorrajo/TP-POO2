e401eb32e00426b51e135687932da5db8d6b1ffd
orianamartina <oriana@camba.coop>
2024-11-03 23:22:42 -0300

    HOTFIX: inquilino rol

--- src/sistema/usuario/Inquilino.java
+++ src/sistema/usuario/Inquilino.java
@@ -3,7 +3,6 @@ package sistema.usuario;
 import sistema.enums.RolDeUsuario;
 
 public class Inquilino extends Usuario {
-	private RolDeUsuario rol;
 	
 	public Inquilino(String nombre, String mail , String telefono) {
 		super(nombre,mail, telefono);
